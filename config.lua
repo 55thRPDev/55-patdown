d2c172f80b09e7fbefd6db37e8a600377b97ed54
55thRPDev <anthonyoneill506@gmail.com>
2023-05-09 00:46:16 -0300

    Added weapons/drugs
    Added more weapons and drugs to config

--- config.lua
+++ config.lua
@@ -3,14 +3,115 @@ Config = {}
 Config.MinDrugs = 20  --Min amount of drugs needed to be detected in patdown
 
 Config.Weapons = {
-    ["weapon_knife"] = true,
+    --Vanilla Weapons
+
+    --Melee
+    ['weapon_dagger'] = true,
+	['weapon_bat'] = true,
+	['weapon_bottle'] = true,
+	['weapon_crowbar'] = true,
+	['weapon_flashlight'] = true,
+	['weapon_golfclub']  = true,
+	['weapon_hammer']  = true,
+	['weapon_hatchet']  = true,
+	['weapon_knuckle']  = true,
+	['weapon_knife']  = true,
+	['weapon_machete']  = true,
+	['weapon_switchblade']  = true,
+	['weapon_nightstick']  = true,
+	['weapon_wrench']  = true,
+
+    --Pistols
     ["weapon_pistol"] = true,
-    ["weapon_glock18c"] = true,
+    ['weapon_pistol_mk2']= true,
+	['weapon_combatpistol'] = true,
+	['weapon_appistol'] = true,
+	['weapon_stungun'] 	= true,
+	['weapon_pistol50'] = true,
+	['weapon_snspistol'] = true,
+	['weapon_heavypistol'] = true,
+	['weapon_vintagepistol'] = true,
+	['weapon_flaregun'] = true,
+	['weapon_marksmanpistol'] = true,
+	['weapon_revolver'] = true,
+	['weapon_revolver_mk2'] = true,
+	['weapon_doubleaction'] = true,
+	['weapon_snspistol_mk2'] = true,
+	['weapon_ceramicpistol']= true,
+	['weapon_navyrevolver']  = true,
+
+    --SMGS
+    ['weapon_microsmg'] = true,
+	['weapon_smg'] = true,
+	['weapon_smg_mk2'] = true,
+	['weapon_assaultsmg']  = true,
+	['weapon_combatpdw'] = true,
+	['weapon_machinepistol'] = true,
+	['weapon_minismg'] = true,
+
+
+    -- Custom Weapons
+	['weapon_de'] = true,
+	['weapon_fnx45'] = true,
+	['weapon_glock17'] = true,
+	['weapon_m9'] = true,
+	['weapon_m1911'] = true,
+	['weapon_uzi'] = true,
+	['weapon_mac10'] = true,
+	['weapon_shiv'] = true,
+	['weapon_ar15'] = true,
+	['weapon_mk14'] = true,
+	['weapon_mp9'] = true,
+	['weapon_glock18c'] = true,
+	['weapon_glock22'] = true,
+	['weapon_karambit'] = true,
+	['weapon_colbaton'] = true,
+
 }
 
 Config.Drugs = {
+    --weed
     ["joint"] = true,
-    ["meth"] = true,
+
+
+    --crack/coke
     ["crack_baggy"] = true,
     ["coke_baggy"] = true,
+    ["crackrock"] = true,
+	["baggedcracked"] = true,
+
+    --heroin
+    ["heroin"] = true,
+    ["heroincut"] = true,
+	["heroinvial"] 	= true,
+	["heroin_ready"] = true,
+
+	-- meth
+	["methtray"] = true,
+    ["meth"] = true,
+
+	-- shrooms
+	["shrooms"] = true,
+
+	-- lean
+	["lean_cup"] = true,
+	["lean_bottle"] = true,
+
+	-- pharma
+	['prescription_pad'] = true,
+	["vicodin_prescription"] = true,
+	["adderal_prescription"] = true,
+	["morphine_prescription"] = true,
+	["xanax_prescription"] 	= true,
+	['adderal'] = true,
+	['vicodin'] = true,
+	['morphine'] = true,
+	['xanax'] = true,
+	['adderalbottle'] = true,
+	['vicodinbottle'] = true,
+	['morphinebottle'] = true,
+	['xanaxbottle'] = true,
+	["emptyvial"] = true,
+	["needle"] 	= true,
+
  }
